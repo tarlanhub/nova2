b9bd418ef3a189dcef85f701c9552c1a8bf87c9c
Tarlan <t17071998@gmail.com>
2021-04-26 01:16:54 +0300

    first commit

---
+++ src/containers/TheHeaderDropdown.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  CBadge,
+  CDropdown,
+  CDropdownItem,
+  CDropdownMenu,
+  CDropdownToggle,
+  CImg
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import {AmplifySignOut} from '@aws-amplify/ui-react'
+
+const TheHeaderDropdown = () => {
+  return (
+    <AmplifySignOut/>
+  )
+}
+
+export default TheHeaderDropdown
